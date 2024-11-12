efc94627fd951a8849b6fccdc7d5c6b626d58898
David Vargas <davids10.dvm@gmail.com>
2024-11-12 16:01:55 -0500

    Feat: basic config files

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.27",
+  networks: {
+    amoy: {
+      url: "https://rpc-amoy.polygon.technology",
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY as string]
+    }
+  }
+};
+
+export default config;
